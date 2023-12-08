62788378f096c092ebcb5d9fcbdc8d8ab233a180
Feng Chen <dbxmcf@gmail.com>
2023-12-08 14:53:15 -0600

    clean up comments

--- recipes/nwchem/7.2.1/sample.mpirun.sh
+++ recipes/nwchem/7.2.1/sample.mpirun.sh
@@ -7,18 +7,7 @@
 #SBATCH -o nw.output.mpirun
 
 module purge
-#export SINGULARITYENV_NWCHEM_BASIS_LIBRARY="/opt/nwchem/share/libraries/"
-#export SLURM_OVERLAP=1
 SIMG="/home/admin/singularity/nwchem7.2.1-openmpi.4.1.4rc2.sif"
-#srun --mpi=pmi2 -n $SLURM_NPROCS singularity exec --bind /work ${SIMG} nwchem input.nw
 module load openmpi
-DIR="/ddnA/work/fchen14/user_errs/sgiri"
 export OMP_NUM_THREADS=1
-#mpirun -n 64 -npernode 32 --wdir $PWD singularity exec --bind /work ${SIMG} nwchem $DIR/input.nw
-#mpirun -n 128 singularity --wdir $PWD exec --bind /work ${SIMG} nwchem $DIR/input.nw
-#mpirun -n 64 -npernode 32 --wdir $DIR singularity exec --bind /work ${SIMG} nwchem input.nw
 mpirun -n 128 singularity exec --bind /ddnA/work ${SIMG} nwchem input.nw
-#mpirun -n 128 singularity exec --bind /ddnA/work ${SIMG} nwchem input.nw
-#mpirun -n 128 singularity exec --bind /ddnA/work ${SIMG} nwchem input.nw
-#singularity exec nwchems.img mpirun --version
-#singularity exec nwchems.img ompi_info
--- recipes/nwchem/7.2.1/sample.srun.sh
+++ recipes/nwchem/7.2.1/sample.srun.sh
@@ -4,11 +4,8 @@
 #SBATCH -t 00:05:00
 #SBATCH -A hpc_hpcadmin9
 #SBATCH -n 128
-#SBATCH -o nw.output
+#SBATCH -o nw.srun.output
 
 module purge
-#export SLURM_OVERLAP=1
 SIMG="/home/admin/singularity/nwchem7.2.1-openmpi.4.1.4rc2.sif"
 srun --mpi=pmi2 -n $SLURM_NPROCS singularity exec --bind /work ${SIMG} nwchem input.nw
-#singularity exec nwchems.img mpirun --version
-#singularity exec nwchems.img ompi_info
