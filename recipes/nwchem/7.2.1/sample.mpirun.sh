10b7a02b2f99aacbcb8d3cd800b5a7e71ac182cf
Feng Chen <dbxmcf@gmail.com>
2023-12-08 15:19:56 -0600

    modified the singularity bind path

--- recipes/nwchem/7.2.1/sample.mpirun.sh
+++ recipes/nwchem/7.2.1/sample.mpirun.sh
@@ -9,5 +9,6 @@
 module purge
 SIMG="/home/admin/singularity/nwchem7.2.1-openmpi.4.1.4rc2.sif"
 module load openmpi
+export SINGULARITY_BIND="/ddnA/work,/ddnA/project"
 export OMP_NUM_THREADS=1
-mpirun -n 128 singularity exec --bind /ddnA/work ${SIMG} nwchem input.nw
+mpirun -n 128 singularity exec ${SIMG} nwchem input.nw
