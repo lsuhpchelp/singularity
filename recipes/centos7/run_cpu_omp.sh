0521c84f339bc3773839caf7894c2e500b1eaf58
Feng Chen <fchen14@lsu.edu>
2025-01-10 14:37:08 -0600

    added openmp example script

---
+++ recipes/centos7/run_cpu_omp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -p checkpt
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 48
+
+module purge
+module load centos7-runner/1.0
+
+# use mvapich2.modules to load necessary modules from rhel7 software stack
+export SINGULARITYENV_MODULE_FILE="$PWD/mvapich2.modules"
+
+#export SINGULARITYENV_OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+#/usr/bin/time centos7run lmp -in in.ser.lj -pk omp $SLURM_CPUS_PER_TASK -sf omp
+/usr/bin/time centos7run lmp -in in.ser.lj -pk omp $SLURM_CPUS_PER_TASK -sf omp
+
+export SINGULARITYENV_OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+#/usr/bin/time centos7run lmp -in in.ser.lj -pk omp $SLURM_CPUS_PER_TASK -sf omp
+/usr/bin/time centos7run lmp -in in.ser.lj -sf omp
+
+/usr/bin/time centos7run lmp -in in.ser.lj -pk omp $SLURM_CPUS_PER_TASK -sf omp
