8c24d2fcc9603f380e9c5c4b0fb81946b9208eaf
Feng Chen <dbxmcf@gmail.com>
2025-08-26 15:04:35 -0500

    added matlab 2025a installation files both the recipe and the host installation scripts

---
+++ recipes/matlab/r2025a/install.host.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./mpm install --inputfile ./mpm_input_r2025a.host.txt
+
---
+++ recipes/matlab/r2025a/install_service_host.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Clone MWSH management scripts
+#git clone https://github.com/mathworks-ref-arch/administer-mathworks-service-host.git
+#
+## admin-controlled service host installation
+cd administer-mathworks-service-host/admin-scripts/linux/admin-controlled-installation
+
+./download_msh.sh --destination ./tmp/Downloads/MathWorks/ServiceHost
+#./install_msh.sh --source ./tmp/Downloads/MathWorks/ServiceHost --destination /opt/matlab_service_host
+./install_msh.sh --source ./tmp/Downloads/MathWorks/ServiceHost --destination /usr/local/packages/license/matlab/matlab_service_host --no-update-environment
+
